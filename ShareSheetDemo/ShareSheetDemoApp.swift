d2ce4ec8b4c693b9ec68558f782bfc2251c7d988
J0na17anT98 <17661536+JTostitos@users.noreply.github.com>
2022-09-01 18:35:06 -0400

    Initial Commit

---
+++ ShareSheetDemo/ShareSheetDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShareSheetDemoApp.swift
+//  ShareSheetDemo
+//
+//  Created by Jonathan Tsistinas on 9/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ShareSheetDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
